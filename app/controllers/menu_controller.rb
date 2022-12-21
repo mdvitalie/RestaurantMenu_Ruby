8bd9d46c6187ea6194235ee98d03cad2ce976920
mdvitalie.ie <mdvitalie@gmail.com>
2022-12-21 13:36:15 +0000

    A second API has been added. Food menu API

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,7 +5,7 @@ require 'net/http'
 
 class MenuController < ApplicationController
   def index
-    # Weather API
+   # Weather API
 
     url = 'https://api.openweathermap.org/data/2.5/weather?lat=53.350140&lon=-6.266155&units=metric&appid=197128ae14d1b8c4d1779e83bd3c628e'
 
@@ -14,5 +14,14 @@ class MenuController < ApplicationController
     # puts res.body if res.is_a?(Net::HTTPSuccess)
     @data = JSON.parse(res.body)
     # console
+
+
+    #Food menu API
+    url2 = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+
+    uri2 = URI(url2)
+    res2 = Net::HTTP.get_response(uri2)
+    # puts res.body if res.is_a?(Net::HTTPSuccess)
+    @foodmenu = JSON.parse(res2.body)
   end
 end
